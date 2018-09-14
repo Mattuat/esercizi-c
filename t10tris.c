2e8c6a5e4f712c5f2fe3f05cf2e81748a3714f20
Mattuat <37597587+Mattuat@users.noreply.github.com>
2018-09-14 17:46:50 +0200

    Add files via upload

--- t10tris.c
+++ t10tris.c
@@ -15,46 +15,148 @@ interno del file.
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
 
 
 sem_t *semp,*semf;
+char *addr;
+FILE *file;
+int fd;
 
+
+void handler(){
+	int start,end;
+	off_t ret2,fileLenght;
+	
+	printf("stop signal received!\n");
+	sleep(1);
+	printf("choose range for reading:\n");
+	
+	init:
+	printf("type initial value for range\n");
+	scanf("%d",&start);
+	
+	final:
+	printf("type final value for range (how many bytes to read)\n");
+	scanf("%d",&end);
+	
+	fileLenght = lseek(fd,0,SEEK_END);
+	
+	ret2 = lseek(fd,start,SEEK_SET);
+	
+	if(ret2 > fileLenght || ret2 == (off_t)-1){
+		printf("choose a valid value for the beginning of range please\n");
+		goto init;
+		}
+	if((ret2 + end) > fileLenght){
+		printf("can't read '%d' bytes: out of file!\n",end);
+		goto final;
+		}
+	
+	char readbuff[end];
+	char *temp;
+	
+	ret2 = fread(readbuff,1,end,file);
+	
+	if(ret2 == 0){
+		printf("read fail!\n");
+		exit(-1);
+		}
+		
+	printf("\n%s",readbuff);
+	
+		
+	printf("\nProgram Closed, goodbye!\n");
+	
+	exit(1);
+	}
 
 void routine(){
+	int ret,k;
 	while(1){
-		printf("while figlio\n");
 		sem_wait(semf);
-		sleep(2);
-		printf("sono figlio %d\n",getpid());
+		
+		sleep(1);
+		
+		printf("\nProcess %d is writing '%s' on file\n",getpid(),addr);
+		
+		ret = fprintf(file,"%d: %s\n",getpid(),addr);
+		
+		if(ret <= 0){
+			printf("writing fail!\n");
+			exit(-1);
+			}
+			
+		fflush(file);
+	
+		for(k=0;k<3;k++){
+			sleep(1);
+			printf("...\n");
+			fflush(stdin);
+			}
+		
+		
+		sleep(1);
+		printf("Message wrote on file!\n");
+		sleep(1);
+		
 		sem_post(semp);
-	}
-}
+		}
+		}
 
 int main(int argc,const char* argv[]){
 
 	if(argc < 3){
-		printf("usage: nomeProg interoN nomeFile");
+		printf("usage: nomeProg interoN nomeFile\n");
 		exit(-1);
 		}
 		
-	int N,i;
+	int N,i,ret;
 	
 	N = atoi(argv[1]);
 	
 	semp = sem_open("father",O_CREAT,0666,1);
-	//sem_unlink("father");
+	sem_unlink("father");
+	
 	if(semp == SEM_FAILED){
-		printf("semp fail!\n");
+		printf("semp_open fail!\n");
 		exit(-1);
 		}
 	 
 	semf = sem_open("child",O_CREAT,0666,0);
-	//sem_unlink("child");
+	sem_unlink("child");
+	
 	if(semf == SEM_FAILED){
-		printf("semf fail!\n");
+		printf("semf_open fail!\n");
 		exit(-1);
 		}
+		
+	addr = (char*)mmap(NULL,4096*5,PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,0,0);
 	
+	if(addr == (void*)-1){
+		printf("mmap fail!\n");
+		exit(-2);
+		}
+	
+	fd = open(argv[2],O_CREAT|O_TRUNC|O_RDWR,0666);
+	
+	if(fd == -1){
+		printf("open fail!\n");
+		exit(-1);
+		}
+		
+	file = fdopen(fd,"r+");
+	
+	if(file == NULL){
+		printf("fdopen fail!\n");
+		exit(-2);
+		}
 	
 	for(i=0;i<N;i++){
 		if(fork() == 0){
@@ -63,14 +165,33 @@ int main(int argc,const char* argv[]){
 		}
 			
 		while(1){
-			printf("while padre\n");
+			
+			signal(SIGUSR1,handler);
+			
+			if(errno == EINVAL){
+				printf("signal error!\n");
+				exit(-1);
+				}
+			
 			sem_wait(semp);
-			sleep(2);
-			printf("so padre\n");
+			
+			printf("\ntype a word, it will be written on file '%s'\n",argv[2]);
+			
+			ret = scanf("%s",addr);
+			
+			if(ret == 0 || ret == EOF){
+				printf("scanf fail!\n");
+				exit(-1);
+				}
+				
+			if(!strcmp(addr,"stop")){
+				raise(SIGUSR1);
+				}
+			
 			sem_post(semf);
-		}
+			}
 	return 0;
-}
+	}
 	
 	
 	
