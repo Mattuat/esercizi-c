bb3c75fab8395f9b6aedafcb3a93e2880d06826c
Mattuat <37597587+Mattuat@users.noreply.github.com>
2018-09-15 14:53:28 +0200

    Add files via upload

--- t11.c
+++ t11.c
@@ -19,6 +19,8 @@ immesse da standard-input e memorizzate nei file destinazione.*/
 #include <sys/types.h>
 #include <fcntl.h>
 #include <string.h>
+#include <signal.h>
+#include <errno.h>
 
 #define MAXSIZE 1024
 
@@ -27,26 +29,80 @@ char messaggio[MAXSIZE];
 sem_t *semf,*semp;
 int i;
 int *fd;
+int N;
 
+void handler(){
+	int k;
+	FILE* file;
+	size_t a;
+	printf("SIGINT received by thread %ld\n",pthread_self());
+	printf("printing files:\n\n");
+	
+	for(k=0;k<N;k++){
+	
+		file = fdopen(fd[k],"r+");
+		
+		printf("file nr %d content:\n",fd[k]-3);
+		
+		rewind(file);
+		
+		if(file == NULL){
+			printf("fdopen error!\n");
+			exit(-1);
+			}
+		printf("\n");
+		while((a = fread(messaggio,1,MAXSIZE,file)) > 0){
+			
+			fwrite(messaggio,1,a,stdout);
+			}
+			fflush(stdout);
+		printf("\n");
+		printf("\n");
+		}
+		
+	printf("end of print, program closed\n");
+	
+	free(fd);
+	free(semp);
+	free(semf);
+	exit(1);
+	}
 
 void* routine(){
 		int j;
 		ssize_t ret;
 		j = i-1;
+		char c[1] = {" "};
+		int rets;
+		
+		rets = signal(SIGINT,handler);
+		if(rets == SIG_ERR || errno == EINVAL){
+			printf("signal error!");
+			exit(-1);
+			}
+		
+		
 			
 		while(1){
 			 
 			 sem_wait(semf);
 			 
-			 printf("thread %ld is in!\n",pthread_self());
+			 printf("thread %ld is writing on file num %d!\n\n",pthread_self(),fd[j]-3);
+			 printf("message is '%s'\n",messaggio);
 			 
-			 ret = write(*(fd+j),messaggio,strlen(messaggio));
-			 
-			 if(ret == -1 || ret < strlen(messaggio)){
-			 	printf("write error\n");
+			 ret = write(fd[j],messaggio,strlen(messaggio));
+			 if(ret == -1 || ret != strlen(messaggio)){
+			 	printf("write1 error!\n");
 			 	exit(-1);
 			 	}
-			 	
+			 
+			 ret = write(fd[j],c,1);
+			 if(ret == -1 || ret != 1){
+			 	printf("write2 error!\n");
+			 	exit(-1);
+			 	}
+			 
+			 
 			 sem_post(semp);
 			 }
 		exit(-1);
@@ -59,10 +115,11 @@ int main(int argc,const char* argv[]){
 		exit(-1);
 		}
 
-	int N;
+	
 	N = argc-1;
 	
 	int ret;
+	
 	
 	
 	fd = malloc(sizeof(int)*N);
@@ -107,9 +164,9 @@ int main(int argc,const char* argv[]){
 	
 	for(i=0;i<N;i++){
 		
-		*(fd + i) = open(argv[i+1],O_TRUNC|O_CREAT|O_RDWR,0666);
+		fd[i] = open(argv[i+1],O_TRUNC|O_CREAT|O_RDWR,0666);
 		
-		if(*(fd+i) == -1){
+		if(fd[i] == -1){
 		printf("open fail!\n");
 		exit(-1);
 		}
@@ -120,11 +177,14 @@ int main(int argc,const char* argv[]){
 			printf("pthread_create error!\n");
 			exit(-1);
 			}
-		
 		}
 		
 	
-		
+		ret = signal(SIGINT,SIG_IGN);
+		if(ret == SIG_ERR || errno == EINVAL){
+			printf("signal error!");
+			exit(-1);
+			}
 	while(1){
 	
 		sem_wait(semp);
